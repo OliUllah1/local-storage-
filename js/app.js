06f5bd32f94621edfe86ac966e82b4b8ed7c70e5
Oli Ulllah <62113494+OliUllah1@users.noreply.github.com>
2023-03-10 22:40:21 +0600

    find local storage data and slow the page

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
+localStorage.clear()
 localStorage.setItem('name','oli')
+
 localStorage.setItem('age',21)
 localStorage.removeItem('name')
 localStorage.removeItem('age')
 const person ={firstName:'oli',age:21};
-console.log(person)
 const info=JSON.stringify(person)
-console.log(info)
 localStorage.setItem('info',info)
+
+const findObjectToLocalStorage = localStorage.getItem('info');
+const convertObject = JSON.parse(findObjectToLocalStorage);
+console.log(convertObject)
+const div =document.getElementById('div')
+const div1 = document.createElement('div');
+div1.innerHTML=`
+<ul>
+<li>${convertObject.firstName}</li>
+<li>${convertObject.age}</li>
+</ul>
+`;
+div.appendChild(div1)
